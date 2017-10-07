2266c0083ffdc13061038dd48509deea1a5f4234
Kathleen K <kkamali@users.noreply.github.com>
2017-10-07 12:56:30 -0400

    More movement changes

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -8,18 +8,17 @@ public class MovementController : MonoBehaviour {
 	public float movementSpeed;
 	public static float micLoudness; 
 	private int sample = 128; 
-	private Rigidbody rb; 
 	private AudioClip clip; 
 
 	void Start () {
-		rb = GetComponent<Rigidbody>(); 
 		clip = Microphone.Start (null, true, 999, 44100); 
 	}
 
 	void FixedUpdate() {
 		micLoudness = LevelMax (); 
 		if (micLoudness > 0.001) {
-			rb.AddForce (cameraRig.transform.forward * movementSpeed); 
+			Debug.Log (micLoudness); 
+			transform.Translate(cameraRig.transform.forward, Time.deltaTime);
 		}
 	}
 		
@@ -33,7 +32,7 @@ public class MovementController : MonoBehaviour {
 		for (int i = 0; i < sample; i++) {
 			float wavePeak = waveData [i] * waveData [i];
 			if (levelMax < wavePeak) {
-				levelMax = wavePeak; 
+				levelMax = wavePeak; 	
 			}
 		}
 		return levelMax; 
