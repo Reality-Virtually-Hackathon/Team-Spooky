bd3a6774bfbd385c9b26f8211d64c17e4d9ea137
Kathleen K <kkamali@users.noreply.github.com>
2017-10-07 12:09:02 -0400

    Movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,15 +5,38 @@ using UnityEngine;
 public class MovementController : MonoBehaviour {
 	
 	public GameObject cameraRig; 
-
+	public float movementSpeed;
+	public static float micLoudness; 
+	private int sample = 128; 
 	private Rigidbody rb; 
+	private AudioClip clip; 
 
 	void Start () {
-		rb = cameraRig.GetComponent<Rigidbody>(); 
+		rb = GetComponent<Rigidbody>(); 
+		clip = Microphone.Start (null, true, 999, 44100); 
 	}
 
-	void Update () {
-		rb.AddRelativeForce(Vector3.forward * 50); 
-		Debug.Log(cameraRig.transform.position);
+	void FixedUpdate() {
+		micLoudness = LevelMax (); 
+		if (micLoudness > 0.001) {
+			rb.AddForce (cameraRig.transform.forward * movementSpeed); 
+		}
 	}
+		
+	float LevelMax() {
+		float levelMax = 0; 
+		float[] waveData = new float[sample];
+		int micPos = Microphone.GetPosition (null) - (sample + 1); 
+		if (micPos < 0)
+			return 0; 
+		clip.GetData (waveData, micPos);
+		for (int i = 0; i < sample; i++) {
+			float wavePeak = waveData [i] * waveData [i];
+			if (levelMax < wavePeak) {
+				levelMax = wavePeak; 
+			}
+		}
+		return levelMax; 
+	}
+
 }
