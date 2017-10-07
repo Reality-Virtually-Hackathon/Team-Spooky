583f151c1c1e510cb893856c3ec043d43edcea9d
Kathleen K <kkamali@users.noreply.github.com>
2017-10-07 15:23:35 -0400

    Changes

---
+++ Assets/Scripts/ArmTrapObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmTrapObject : MonoBehaviour {
+    public GameObject myTrapToArm;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Interact()
+    {
+        myTrapToArm.GetComponent<ProximityObject>().Armed();
+       
+    }
+}
---
+++ Assets/Scripts/ProximityObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityObject : MonoBehaviour {
+    public bool armed;
+    public float totalCountdown;
+    public float countdown;
+
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (armed == true)
+        { countdown -= Time.deltaTime; }
+        if (countdown <= 0) { armed = false;  }
+	}
+
+    public void OnTriggerEnter(Collider col)
+    { if (col.gameObject.tag == "Kid" && armed == true) { ActivateScare(); } }
+
+
+    public void ActivateScare()
+    {
+        armed = false;
+        anim.Play("Pop");
+  
+    }
+    public void Armed()
+    {
+        countdown = totalCountdown;
+        armed = true;
+        
+    }
+}
